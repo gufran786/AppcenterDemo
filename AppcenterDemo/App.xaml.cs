4335533be56c9610c0bc975f57a9a2b9af7b1217
Gufran Ali <gufran@192.168.1.4>
2020-10-20 16:29:30 +0530

    project working

---
+++ AppcenterDemo/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace AppcenterDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=6ce5715d-e617-4374-81d5-cc11d924c721;" +
+                  "ios=1584627b-9437-4f6e-a12c-db7025b4ea03",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
